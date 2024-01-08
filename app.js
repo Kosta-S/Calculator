ac1ba72aa26d3eec1d1d96ae8599f9e0d04a08de
Kosta Shlyak <constantineandi@gmail.com>
2024-01-08 08:50:30 +0300

    HTML and basic CSS, added script to button delete and digits

--- app.js
+++ app.js
@@ -24,3 +24,31 @@ let operate =(operator, numberOne, numberTwo) => {
     operator ==='*'? multiply(numberOne, numberTwo):
     operator ==='/'? divide(numberOne, numberTwo): null;
     };
+
+    let calcButtons = document.querySelectorAll(".row");
+    let inputField = document.querySelector('#input');
+    
+    calcButtons.forEach(buttonRow => {
+        buttonRow.addEventListener('click', function(e) {
+            if (e.target.classList.contains('digit') && inputField.innerText === '0') {
+                inputField.innerText = e.target.innerText;
+            } else if (e.target.classList.contains('digit')){
+                inputField.innerText += e.target.innerText;
+            }
+        });
+    });
+
+    let clearButton = document.getElementById('clear');
+
+    clearButton.addEventListener('click', function(e){
+        inputField.innerText = 0;
+    })
+
+    let deleteButton = document.getElementById('delete');
+
+    deleteButton.addEventListener('click', function(e){
+        let inputText = inputField.innerText;
+        if (inputText.length > 0) {
+            let newInputText = inputText.slice(0, -1);
+            inputField.innerText = newInputText;
+    }});
